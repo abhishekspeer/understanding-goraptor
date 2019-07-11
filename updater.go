b233c26174a3b60986d21543910baa6f15905828
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2019-07-11 09:37:53 +0530

    Update updator

--- updater.go
+++ updater.go
@@ -1,20 +1,75 @@
 package main
 
 import (
-	"regexp"
+	"fmt"
 	"strings"
-	"time"
+
+	"github.com/deltamobile/goraptor"
 )
 
-func update(pt *string) updater {
+type updater func(goraptor.Term) error
+
+// takes in string updates string
+func update(val *ValueStr) updater {
 	key := false
 	return func(term goraptor.Term) error {
-		if set {
+		if key {
+			return fmt.Errorf("Property Already Defined")
+		}
+		val.Val = termStr(term)
+		key = true
+		return nil
+	}
+}
+func updateTrimPrefix(prefix string, ptr *ValueStr) updater {
+	key := false
+	return func(term goraptor.Term) error {
+		if key {
 			return fmt.Errorf("Property Already Defined")
 		}
 
-		pt = termStr(term)
+		ptr.Val = strings.TrimPrefix(termStr(term), prefix)
 		key = true
+		return nil
+	}
+}
+
+func updateList(sl *[]string) updater {
+	return func(term goraptor.Term) error {
+		*sl = append(*sl, termStr(term))
+		return nil
+	}
+}
+
+// Update a ValueCreator pointer
+func updateCreator(ptr *ValueCreator) updater {
+	key := false
+	return func(term goraptor.Term) error {
+		if key {
+			return fmt.Errorf("Property Already Defined")
+		}
+		ptr.SetValue(termStr(term))
+		key = true
+		return nil
+	}
+}
+
+// Update a ValueDate pointer
+func updDate(ptr *ValueDate) updater {
+	key := false
+	return func(term goraptor.Term) error {
+		if key {
+			return fmt.Errorf("Property Already Defined")
+		}
+		ptr.SetValue(termStr(term))
+		key = true
+		return nil
+	}
+}
+
+func updListCreator(sl *[]ValueCreator) updater {
+	return func(term goraptor.Term) error {
+		*sl = append(*sl, ValueCreatorNew(termStr(term)))
 		return nil
 	}
 }
