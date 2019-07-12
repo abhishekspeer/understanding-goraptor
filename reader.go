441322888c70b4ea830ce83ed4147796f2214a22
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2019-07-12 16:22:54 +0530

    Undo Refactor

--- reader.go
+++ reader.go
@@ -2,9 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/deltamobile/goraptor"
 )
+
+func main() {
+
+	// check that we've received the right number of arguments
+
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Printf("Usage: %v <spdx-file-in>\n", args[0])
+		fmt.Printf("  Load SPDX 2.1 RDF file <spdx-file-in>, and\n")
+		fmt.Printf("  print its contents.\n")
+		return
+	}
+
+	//   PARSE FILE method - Takes the file location as an input
+	input := args[1]
+	parserfile := NewParser(input)
+	defer parserfile.Free()
+
+	// Parse the file using goraptor's ParseFile method and return a Statement.
+	ch := parserfile.rdfparser.ParseFile(input, "") // takes in input and baseuri
+	for {
+		statement, ok := <-ch
+		// fmt.Println(statement)
+		if !ok {
+			break
+		}
+		err := parserfile.processTriple(statement)
+		if err != nil {
+			fmt.Println("Processing Failed")
+		}
+	}
+
+	parserinstance := parserfile
+	// fmt.Println(parserinstance.buffer)
+	fmt.Println(len(parserinstance.buffer))
+}
 
 var (
 	URInsType = uri("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
@@ -212,3 +249,5 @@ func literal(lit string) *goraptor.Literal {
 func blank(b string) *goraptor.Blank {
 	return (*goraptor.Blank)(&b)
 }
+
+type updater func(goraptor.Term) error
