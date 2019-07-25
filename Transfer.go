9586a9c8f2a0f237a57afb026a5aca92c232504d
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2019-07-25 21:52:08 +0530

    WIP

--- Transfer.go
+++ Transfer.go
@@ -5,39 +5,39 @@ import (
 	"ug/understanding-goraptor/rdf2v1"
 )
 
-func transferDocument(spdxdoc *rdf2v1.Document) *spdx.Document2_1 {
+// func transferDocument(spdxdoc *rdf2v1.Document) *spdx.Document2_1 {
 
-	stdDoc := spdx.Document2_1{
+// 	stdDoc := spdx.Document2_1{
 
-		CreationInfo:  transferCreationInfo(spdxdoc),
-		Packages:      transferPackages(spdxdoc),
-		OtherLicenses: transferOtherLicenses(spdxdoc),
-		Relationships: transferRelationships(spdxdoc),
-		Annotations:   transferAnnotation(spdxdoc),
-		Reviews:       transferReviews(spdxdoc),
-	}
-	return &stdDoc
-}
+// 		CreationInfo:  transferCreationInfo(spdxdoc),
+// 		Packages:      transferPackages(spdxdoc),
+// 		OtherLicenses: transferOtherLicenses(spdxdoc),
+// 		Relationships: transferRelationships(spdxdoc),
+// 		Annotations:   transferAnnotation(spdxdoc),
+// 		Reviews:       transferReview(spdxdoc),
+// 	}
+// 	return &stdDoc
+// }
 
 func transferCreationInfo(spdxdoc *rdf2v1.Document) *spdx.CreationInfo2_1 {
 
-	var list []string
-	list = append(list, spdxdoc.ExternalDocumentRef.ExternalDocumentId.V())
-	list = append(list, spdxdoc.ExternalDocumentRef.SPDXDocument.V())
-	list = append(list, spdxdoc.ExternalDocumentRef.Checksum.Algorithm.V())
+	var listExtDocRef []string
+	listExtDocRef = append(listExtDocRef, spdxdoc.ExternalDocumentRef.ExternalDocumentId.V())
+	listExtDocRef = append(listExtDocRef, spdxdoc.ExternalDocumentRef.SPDXDocument.V())
+	listExtDocRef = append(listExtDocRef, spdxdoc.ExternalDocumentRef.Checksum.Algorithm.V())
 
 	stdCi := spdx.CreationInfo2_1{
 
-		SPDXVersion:                spdxdoc.SPDXVersion.V(),
-		DataLicense:                spdxdoc.DataLicense.V(),
+		SPDXVersion:                spdxdoc.SPDXVersion.Val,
+		DataLicense:                spdxdoc.DataLicense.Val,
 		SPDXIdentifier:             "",
-		DocumentName:               spdxdoc.DocumentName.V(),
+		DocumentName:               spdxdoc.DocumentName.Val,
 		DocumentNamespace:          "",
-		ExternalDocumentReferences: list,
+		ExternalDocumentReferences: listExtDocRef,
 		LicenseListVersion:         "",
-		Created:                    spdxdoc.CreationInfo.Create.Val(),
-		CreatorComment:             spdxdoc.CreationInfo.Comment.V(),
-		DocumentComment:            spdxdoc.DocumentComment.V(),
+		Created:                    spdxdoc.CreationInfo.Create.Val,
+		CreatorComment:             spdxdoc.CreationInfo.Comment.Val,
+		DocumentComment:            spdxdoc.DocumentComment.Val,
 	}
 	return &stdCi
 }
@@ -47,7 +47,7 @@ func transferAnnotation(spdxdoc *rdf2v1.Document) *spdx.Annotation2_1 {
 	stdAnn := spdx.Annotation2_1{
 		Annotator:                spdxdoc.Annotation.Annotator.Val,
 		AnnotationType:           spdxdoc.Annotation.AnnotationType.Val,
-		AnnotationDate:           spdxdoc.Annotation.AnnotationDate.Val(),
+		AnnotationDate:           spdxdoc.Annotation.AnnotationDate.Val,
 		AnnotationComment:        spdxdoc.Annotation.AnnotationComment.Val,
 		AnnotationSPDXIdentifier: "",
 		AnnotatorType:            "",
@@ -57,17 +57,17 @@ func transferAnnotation(spdxdoc *rdf2v1.Document) *spdx.Annotation2_1 {
 }
 
 // Discuss location
-func transferReview(spdxdoc *rdf2v1.Document) *[]spdx.Review2_1 {
+// func transferReview(spdxdoc *rdf2v1.Document) *([]spdx.Review2_1) {
 
-	stdRev := spdx.Review2_1{
-		Reviewer:      spdxdoc.Review.Reviewer.Val,
-		ReviewerType:  "",
-		ReviewDate:    spdxdoc.Review.ReviewDate.Val,
-		ReviewComment: spdxdoc.Review.ReviewComment.Val,
-	}
+// 	stdRev := spdx.Review2_1{
+// 		Reviewer:      spdxdoc.Review.Reviewer.Val,
+// 		ReviewerType:  "",
+// 		ReviewDate:    spdxdoc.Review.ReviewDate.Val,
+// 		ReviewComment: spdxdoc.Review.ReviewComment.Val,
+// 	}
 
-	return &stdRev
-}
+// 	return &stdRev
+// }
 
 func transferFile(spdxdoc *rdf2v1.Document) []*spdx.File2_1 {
 	var arrFile []spdx.File2_1
@@ -80,18 +80,18 @@ func transferFile(spdxdoc *rdf2v1.Document) []*spdx.File2_1 {
 
 				FileName:           file.FileName.Val,
 				FileSPDXIdentifier: "",
-				// FileType:           file.FileType.,
+				FileType:           rdf2v1.ValueList(file.FileType),
 				FileChecksumSHA1:   "",
 				FileChecksumSHA256: "",
 				FileChecksumMD5:    "",
 				// LicenseConcluded:   "",
-				// LicenseInfoInFile:  file.LicenseInfoInFile.Val(),
+				LicenseInfoInFile: rdf2v1.ValueList(file.LicenseInfoInFile),
 				LicenseComments:   file.FileLicenseComments.V(),
 				FileCopyrightText: file.FileCopyrightText.V(),
 				// ArtifactOfProjects: file.Annotation.Annotator.Val,
-				FileComment: file.FileComment.Val,
-				FileNotice:  file.FileNoticeText.Val,
-				// FileContributor:    file.Annotation.AnnotationComment.Val,
+				FileComment:     file.FileComment.Val,
+				FileNotice:      file.FileNoticeText.Val,
+				FileContributor: rdf2v1.ValueList(file.FileContributor),
 				// FileDependencies:   "",//DISCUSS
 				// Snippets:           "",//DISCUSS
 			}
@@ -101,7 +101,7 @@ func transferFile(spdxdoc *rdf2v1.Document) []*spdx.File2_1 {
 	return arrFile
 }
 
-func transferPackages(spdxdoc *rdf2v1.Document) *[]spdx.Package2_1 {
+func transferPackages(spdxdoc *rdf2v1.Document) *[]*spdx.Package2_1 {
 	var arrPkg []spdx.Package2_1
 	for i, _ := range spdxdoc.Relationship {
 		k := spdxdoc.Relationship[i]
@@ -161,4 +161,23 @@ func transferOtherLicenses(spdxdoc *rdf2v1.Document) []*spdx.OtherLicense2_1 {
 	}
 
 	return &stdOl
+}
+
+func transferArtifactOfProject(spdxdoc *rdf2v1.Document) *spdx.ArtifactOfProject2_1 {
+	// var stdAop spdx.ArtifactOfProject2_1
+	for i, _ := range spdxdoc.Relationship {
+		k := spdxdoc.Relationship[i]
+		for _, b := range k.File {
+			file := b
+
+			stdAop := spdx.ArtifactOfProject2_1{
+				Name:     file.Project.Name.Val,
+				HomePage: file.Project.Homepage.Val,
+				URI:      "",
+			}
+
+			return &stdAop
+		}
+	}
+	return nil
 }
