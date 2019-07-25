e8865f5bc4d1ce7768203eed239c19460045831a
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2019-07-25 19:20:05 +0530

    WIP Transfer.go

--- Transfer.go
+++ Transfer.go
@@ -5,7 +5,21 @@ import (
 	"ug/understanding-goraptor/rdf2v1"
 )
 
-func transferCreationInfo(spdxdoc *rdf2v1.Document) spdx.CreationInfo2_1 {
+func transferDocument(spdxdoc *rdf2v1.Document) *spdx.Document2_1 {
+
+	stdDoc := spdx.Document2_1{
+
+		CreationInfo:  transferCreationInfo(spdxdoc),
+		Packages:      transferPackages(spdxdoc),
+		OtherLicenses: transferOtherLicenses(spdxdoc),
+		Relationships: transferRelationships(spdxdoc),
+		Annotations:   transferAnnotation(spdxdoc),
+		Reviews:       transferReviews(spdxdoc),
+	}
+	return &stdDoc
+}
+
+func transferCreationInfo(spdxdoc *rdf2v1.Document) *spdx.CreationInfo2_1 {
 
 	var list []string
 	list = append(list, spdxdoc.ExternalDocumentRef.ExternalDocumentId.V())
@@ -15,7 +29,7 @@ func transferCreationInfo(spdxdoc *rdf2v1.Document) spdx.CreationInfo2_1 {
 	stdCi := spdx.CreationInfo2_1{
 
 		SPDXVersion:                spdxdoc.SPDXVersion.V(),
-		DataLicense:                spdxdoc.CreationInfo.V(),
+		DataLicense:                spdxdoc.DataLicense.V(),
 		SPDXIdentifier:             "",
 		DocumentName:               spdxdoc.DocumentName.V(),
 		DocumentNamespace:          "",
@@ -25,15 +39,10 @@ func transferCreationInfo(spdxdoc *rdf2v1.Document) spdx.CreationInfo2_1 {
 		CreatorComment:             spdxdoc.CreationInfo.Comment.V(),
 		DocumentComment:            spdxdoc.DocumentComment.V(),
 	}
-	return stdCi
+	return &stdCi
 }
 
-func transferAnnotation(spdxdoc *rdf2v1.Document) spdx.Annotation2_1 {
-
-	var list []string
-	list = append(list, spdxdoc.ExternalDocumentRef.ExternalDocumentId.V())
-	list = append(list, spdxdoc.ExternalDocumentRef.SPDXDocument.V())
-	list = append(list, spdxdoc.ExternalDocumentRef.Checksum.Algorithm.V())
+func transferAnnotation(spdxdoc *rdf2v1.Document) *spdx.Annotation2_1 {
 
 	stdAnn := spdx.Annotation2_1{
 		Annotator:                spdxdoc.Annotation.Annotator.Val,
@@ -44,5 +53,112 @@ func transferAnnotation(spdxdoc *rdf2v1.Document) spdx.Annotation2_1 {
 		AnnotatorType:            "",
 	}
 
-	return stdAnn
+	return &stdAnn
+}
+
+// Discuss location
+func transferReview(spdxdoc *rdf2v1.Document) *[]spdx.Review2_1 {
+
+	stdRev := spdx.Review2_1{
+		Reviewer:      spdxdoc.Review.Reviewer.Val,
+		ReviewerType:  "",
+		ReviewDate:    spdxdoc.Review.ReviewDate.Val,
+		ReviewComment: spdxdoc.Review.ReviewComment.Val,
+	}
+
+	return &stdRev
+}
+
+func transferFile(spdxdoc *rdf2v1.Document) []*spdx.File2_1 {
+	var arrFile []spdx.File2_1
+	for i, _ := range spdxdoc.Relationship {
+		k := spdxdoc.Relationship[i]
+		for _, b := range k.File {
+			file := b
+
+			stdFile := spdx.File2_1{
+
+				FileName:           file.FileName.Val,
+				FileSPDXIdentifier: "",
+				// FileType:           file.FileType.,
+				FileChecksumSHA1:   "",
+				FileChecksumSHA256: "",
+				FileChecksumMD5:    "",
+				// LicenseConcluded:   "",
+				// LicenseInfoInFile:  file.LicenseInfoInFile.Val(),
+				LicenseComments:   file.FileLicenseComments.V(),
+				FileCopyrightText: file.FileCopyrightText.V(),
+				// ArtifactOfProjects: file.Annotation.Annotator.Val,
+				FileComment: file.FileComment.Val,
+				FileNotice:  file.FileNoticeText.Val,
+				// FileContributor:    file.Annotation.AnnotationComment.Val,
+				// FileDependencies:   "",//DISCUSS
+				// Snippets:           "",//DISCUSS
+			}
+			arrFile = append(arrFile, stdFile)
+		}
+	}
+	return arrFile
+}
+
+func transferPackages(spdxdoc *rdf2v1.Document) *[]spdx.Package2_1 {
+	var arrPkg []spdx.Package2_1
+	for i, _ := range spdxdoc.Relationship {
+		k := spdxdoc.Relationship[i]
+		for _, b := range k.Package {
+			pkg := b
+
+			stdPkg := spdx.Package2_1{
+				// IsUnpackaged: "",
+				PackageName:           pkg.PackageName.Val,
+				PackageSPDXIdentifier: "",
+				PackageVersion:        pkg.PackageVersionInfo.Val,
+				PackageFileName:       pkg.PackageFileName.Val,
+
+				PackageSupplierPerson:       "",
+				PackageSupplierOrganization: "",
+				// PackageSupplierNOASSERTION:  "",
+
+				PackageOriginatorPerson:       "",
+				PackageOriginatorOrganization: "",
+				// PackageOriginatorNOASSERTION:  "",
+
+				PackageDownloadLocation: pkg.PackageDownloadLocation.Val,
+				// FilesAnalyzed:                      "",
+				// IsFilesAnalyzedTagPresent:          "",
+				PackageVerificationCode:             pkg.Annotation.AnnotationComment.Val,
+				PackageVerificationCodeExcludedFile: "", //DISCUSS
+				PackageChecksumSHA1:                 pkg.PackageName.Val,
+				PackageChecksumSHA256:               "",
+				PackageChecksumMD5:                  "",
+				PackageHomePage:                     "",
+				PackageSourceInfo:                   "",
+				PackageLicenseConcluded:             "",
+				// PackageLicenseInfoFromFiles: "",
+				PackageLicenseDeclared: pkg.PackageLicenseDeclared.Val,
+				PackageLicenseComments: pkg.PackageLicenseComments.Val,
+				PackageCopyrightText:   pkg.PackageCopyrightText.Val,
+				PackageSummary:         pkg.PackageSummary.Val,
+				PackageDescription:     pkg.PackageDescription.Val,
+				PackageComment:         "", //not in rdf
+				// PackageExternalReferences:   "",
+				Files: transferFile(spdxdoc),
+			}
+			arrPkg = append(arrPkg, stdPkg)
+		}
+	}
+	return &arrPkg
+}
+
+func transferOtherLicenses(spdxdoc *rdf2v1.Document) []*spdx.OtherLicense2_1 {
+
+	stdOl := spdx.Review2_1{
+		LicenseIdentifier:      spdxdoc.Review.Reviewer.Val,
+		ExtractedText:          "",
+		LicenseName:            spdxdoc.Review.ReviewDate.Val,
+		LicenseCrossReferences: spdxdoc.Review.ReviewComment.Val,
+		LicenseComment:         "",
+	}
+
+	return &stdOl
 }
