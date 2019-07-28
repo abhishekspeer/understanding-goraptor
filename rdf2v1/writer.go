49b054f7ae2df237ca830a2006f5b6501138c825
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2019-07-29 01:53:31 +0530

    Add method to add rdf triples

--- rdf2v1/writer.go
+++ rdf2v1/writer.go
@@ -2,17 +2,19 @@ package rdf2v1
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/deltamobile/goraptor"
 )
 
+// Formatter struct to write the output
 type Formatter struct {
 	serializer *goraptor.Serializer
 	nodeIds    map[string]int
 	fileIds    map[string]goraptor.Term
 }
 
-// Initialses a new Formatter Interface
+// NewFormatter initialses a new Formatter Interface
 func NewFormatter(output *os.File, format string) *Formatter {
 
 	// a new goraptor.NewSerializer
@@ -35,7 +37,30 @@ func NewFormatter(output *os.File, format string) *Formatter {
 	}
 }
 
-// Free the serializer
+// NodeId method to set new node ID for a particular prefix
+func (f *Formatter) NodeId(prefix string) *goraptor.Blank {
+
+	f.nodeIds[prefix]++
+
+	id := goraptor.Blank(prefix + strconv.Itoa(f.nodeIds[prefix]))
+	return &id
+}
+
+// Sets node type
+func (f *Formatter) setNodeType(node goraptor.Term, t string) error {
+	return f.add(node, prefix("ns:type"), prefix(t))
+}
+
+// Add 'keys' to 'values' for subject 'to'
+func (f *Formatter) add(to, key, value goraptor.Term) error {
+	return f.serializer.Add(&goraptor.Statement{
+		Subject:   to,
+		Predicate: key,
+		Object:    value,
+	})
+}
+
+// Close to free the serializer
 func (f *Formatter) Close() {
 	f.serializer.EndStream()
 	f.serializer.Free()
