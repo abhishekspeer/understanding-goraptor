a26fcba2a6f1021013d5b3d7b029b0167e186138
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2019-08-13 13:17:12 +0530

    WIP

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	// check that we've received the right number of arguments
 
+	// check that we've received the right number of arguments
 	args := os.Args
 	if len(args) != 2 {
 		fmt.Printf("Usage: %v <spdx-file-in>\n", args[0])
@@ -18,7 +18,7 @@ func main() {
 		return
 	}
 	var spdxdoc *rdf2v1.Document
-	var sp []*rdf2v1.Snippet
+	var sp *rdf2v1.Snippet
 	var err error
 
 	input := args[1]
@@ -28,45 +28,22 @@ func main() {
 		fmt.Println("Parsing Error")
 		return
 	}
-
-	// fmt.Println("===================================================\n")
-	// fmt.Println("Some Information Printed from the Document Returned\n")
-	// fmt.Println("===================================================\n")
-	fmt.Printf("%T\n\n", sp)
-	// // fmt.Printf("Relationship: %v\n\n", spdxdoc.Relationship[0].Package[0])
-	// fmt.Printf("\nRelationship: %v\n\n", spdxdoc.Relationship[3].File)
-	// // fmt.Printf("Relationship: %v\n\n", spdxdoc.Relationship[2])
-	// // fmt.Printf("Relationship: %v\n\n", spdxdoc.Relationship[3])
-	// // fmt.Printf("SpecVersion: %v\n\n", spdxdoc.SPDXVersion.Val)
-	// fmt.Printf("\n\nCreationInfo Creator: %v\n\n", spdxdoc.CreationInfo)
-	// fmt.Printf("CreationInfo Create:%v\n\n", spdxdoc.CreationInfo.Create)
-	// fmt.Printf("DocumentName: %v\n\n", spdxdoc.DocumentName.Val)
-	// fmt.Printf("DocumentComment: %v\n\n", spdxdoc.DocumentComment)
-
-	// iniEdr := spdxdoc.ExternalDocumentRef
-	// intRel := spdxdoc.Relationship
-	// stdRel := make([]spdx.Relationship2_1, 4)
-	// fmt.Printf("Docummment: %v\n\n", stdRel[0])
-
-	fmt.Println("\n\nFINAL TRANSLATED DOCUMENT")
-	// fmt.Printf("%v\n", spdxdoc.DocumentNamespace)
-	// var doc2v1 *spdx.Document2_1
 	doc2v1 := TransferDocument(spdxdoc, sp)
 	fmt.Printf("%T\n\n\n", doc2v1)
 
 	// WRITER
 	output := os.Stdout
-	err = rdf2v1.Write(output, spdxdoc)
+	err = rdf2v1.Write(output, spdxdoc, sp)
 
 }
 
-func Parse(input string) (*rdf2v1.Document, []*rdf2v1.Snippet, error) {
+func Parse(input string) (*rdf2v1.Document, *rdf2v1.Snippet, error) {
 	parser := rdf2v1.NewParser(input)
 	defer fmt.Printf("RDF Document parsed successfully.\n")
 	defer parser.Free()
 	return parser.Parse()
 }
-func TransferDocument(spdxdoc *rdf2v1.Document, sp []*rdf2v1.Snippet) *spdx.Document2_1 {
+func TransferDocument(spdxdoc *rdf2v1.Document, sp *rdf2v1.Snippet) *spdx.Document2_1 {
 
 	stdDoc := spdx.Document2_1{
 
@@ -80,6 +57,20 @@ func TransferDocument(spdxdoc *rdf2v1.Document, sp []*rdf2v1.Snippet) *spdx.Docu
 	return &stdDoc
 }
 
+// func CollectDocument(doc2v1 *spdx.Document2_1, sp []*rdf2v1.Snippet) *rdf2v1.Document {
+
+// 	stdDoc := rdf2v1.Document{
+
+// 		CreationInfo:  collectCreationInfo(doc2v1),
+// 		Packages:      transferPackages(spdxdoc, sp),
+// 		OtherLicenses: transferOtherLicenses(spdxdoc),
+// 		Relationships: transferRelationships(spdxdoc),
+// 		Annotations:   transferAnnotation(spdxdoc),
+// 		Review:        collectReview(doc2v1),
+// 	}
+// 	return &stdDoc
+// }
+
 func transferCreationInfo(spdxdoc *rdf2v1.Document) *spdx.CreationInfo2_1 {
 
 	var listExtDocRef []string
@@ -91,9 +82,9 @@ func transferCreationInfo(spdxdoc *rdf2v1.Document) *spdx.CreationInfo2_1 {
 
 		SPDXVersion:                spdxdoc.SPDXVersion.Val,
 		DataLicense:                spdxdoc.DataLicense.Val,
-		SPDXIdentifier:             "",
+		SPDXIdentifier:             rdf2v1.ExtractId(spdxdoc.DocumentNamespace.Val),
 		DocumentName:               spdxdoc.DocumentName.Val,
-		DocumentNamespace:          "",
+		DocumentNamespace:          spdxdoc.DocumentNamespace.Val,
 		ExternalDocumentReferences: listExtDocRef,
 		LicenseListVersion:         spdxdoc.CreationInfo.LicenseListVersion.Val,
 		CreatorPersons:             rdf2v1.ExtractCreator(spdxdoc.CreationInfo, "Person"),
@@ -102,6 +93,19 @@ func transferCreationInfo(spdxdoc *rdf2v1.Document) *spdx.CreationInfo2_1 {
 		Created:                    spdxdoc.CreationInfo.Create.Val,
 		CreatorComment:             spdxdoc.CreationInfo.Comment.Val,
 		DocumentComment:            spdxdoc.DocumentComment.Val,
+	}
+	return &stdCi
+}
+
+func collectCreationInfo(doc2v1 *spdx.Document2_1) *rdf2v1.CreationInfo {
+
+	stdCi := rdf2v1.CreationInfo{
+
+		SPDXIdentifier:     (rdf2v1.InsertId(doc2v1.CreationInfo.SPDXIdentifier)),
+		LicenseListVersion: rdf2v1.Str(doc2v1.CreationInfo.LicenseListVersion),
+		Creator:            rdf2v1.InsertCreator(doc2v1.CreationInfo),
+		Create:             rdf2v1.Str(doc2v1.CreationInfo.Created),
+		Comment:            rdf2v1.Str(doc2v1.CreationInfo.CreatorComment),
 	}
 	return &stdCi
 }
@@ -172,6 +176,71 @@ func transferAnnotation(spdxdoc *rdf2v1.Document) []*spdx.Annotation2_1 {
 	return arrAnn
 }
 
+// func collectAnnotation(rdf2v1 *spdx.Document2_1) []*rdf2v1.Annotation {
+// 	var arrAnn []*spdx.Annotation2_1
+// 	for _, an := range spdxdoc.Annotation {
+// 		stdAnn := spdx.Annotation2_1{
+// 			Annotator:                rdf2v1.ExtractKeyValue(an.Annotator.Val, "subvalue"),
+// 			AnnotatorType:            rdf2v1.ExtractKeyValue(an.Annotator.Val, "subkey"),
+// 			AnnotationType:           an.AnnotationType.Val,
+// 			AnnotationDate:           an.AnnotationDate.Val,
+// 			AnnotationComment:        an.AnnotationComment.Val,
+// 			AnnotationSPDXIdentifier: "",
+// 		}
+// 		pointer := &stdAnn
+// 		arrAnn = append(arrAnn, pointer)
+// 	}
+
+// 	for _, a := range spdxdoc.Relationship {
+// 		if a != nil {
+// 			if a.Package != nil {
+// 				for _, b := range a.Package {
+// 					if b != nil {
+// 						for _, an := range b.Annotation {
+
+// 							stdAnn := spdx.Annotation2_1{
+// 								Annotator:                rdf2v1.ExtractKeyValue(an.Annotator.Val, "subvalue"),
+// 								AnnotatorType:            rdf2v1.ExtractKeyValue(an.Annotator.Val, "subkey"),
+// 								AnnotationType:           an.AnnotationType.Val,
+// 								AnnotationDate:           an.AnnotationDate.Val,
+// 								AnnotationComment:        an.AnnotationComment.Val,
+// 								AnnotationSPDXIdentifier: "",
+// 							}
+// 							pointer := &stdAnn
+// 							arrAnn = append(arrAnn, pointer)
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+
+// 	}
+// 	for _, a := range spdxdoc.Relationship {
+// 		if a != nil {
+// 			if a.File != nil {
+// 				for _, b := range a.File {
+// 					if b != nil {
+// 						for _, an := range b.Annotation {
+
+// 							stdAnn := spdx.Annotation2_1{
+// 								Annotator:                rdf2v1.ExtractKeyValue(an.Annotator.Val, "subvalue"),
+// 								AnnotatorType:            rdf2v1.ExtractKeyValue(an.Annotator.Val, "subkey"),
+// 								AnnotationType:           an.AnnotationType.Val,
+// 								AnnotationDate:           an.AnnotationDate.Val,
+// 								AnnotationComment:        an.AnnotationComment.Val,
+// 								AnnotationSPDXIdentifier: "",
+// 							}
+// 							pointer := &stdAnn
+// 							arrAnn = append(arrAnn, pointer)
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+
+// 	}
+// 	return arrAnn
+// }
 func transferReview(spdxdoc *rdf2v1.Document) []*spdx.Review2_1 {
 	var arrRev []*spdx.Review2_1
 	for _, a := range spdxdoc.Review {
@@ -181,6 +250,23 @@ func transferReview(spdxdoc *rdf2v1.Document) []*spdx.Review2_1 {
 				ReviewerType:  rdf2v1.ExtractKey(a.Reviewer.Val),
 				ReviewDate:    a.ReviewDate.Val,
 				ReviewComment: a.ReviewComment.Val,
+			}
+			pointer := &stdRev
+			arrRev = append(arrRev, pointer)
+		}
+	}
+
+	return arrRev
+}
+
+func collectReview(doc2v1 *spdx.Document2_1) []*rdf2v1.Review {
+	var arrRev []*rdf2v1.Review
+	for _, a := range doc2v1.Reviews {
+		if a != nil {
+			stdRev := rdf2v1.Review{
+				Reviewer:      rdf2v1.Str(a.Reviewer),
+				ReviewDate:    rdf2v1.Str(a.ReviewDate),
+				ReviewComment: rdf2v1.Str(a.ReviewComment),
 			}
 			pointer := &stdRev
 			arrRev = append(arrRev, pointer)
@@ -205,7 +291,24 @@ func transferRelationships(spdxdoc *rdf2v1.Document) []*spdx.Relationship2_1 {
 
 	return arrRel
 }
-func transferFile(spdxdoc *rdf2v1.Document, sp []*rdf2v1.Snippet) []*spdx.File2_1 {
+
+// Complete it later
+// func collectRelationships(doc2v1 *spdx.Document2_1) []*rdf2v1.Relationship {
+// 	var arrRel []*rdf2v1.Relationship
+// 	for _, a := range doc2v1.Relationships {
+// 		if a != nil {
+// 			stdRel := rdf2v1.Relationship{
+// 				Relationship:        a.RelationshipType.Val,
+// 				RelationshipComment: a.RelationshipComment.Val,
+// 			}
+// 			pointer := &stdRel
+// 			arrRel = append(arrRel, pointer)
+// 		}
+// 	}
+
+// 	return arrRel
+// }
+func transferFile(spdxdoc *rdf2v1.Document, sp *rdf2v1.Snippet) []*spdx.File2_1 {
 	var arrFile []*spdx.File2_1
 	for _, a := range spdxdoc.Relationship {
 		if a != nil {
@@ -241,7 +344,8 @@ func transferFile(spdxdoc *rdf2v1.Document, sp []*rdf2v1.Snippet) []*spdx.File2_
 	return arrFile
 }
 
-func transferPackages(spdxdoc *rdf2v1.Document, sp []*rdf2v1.Snippet) []*spdx.Package2_1 {
+
+func transferPackages(spdxdoc *rdf2v1.Document, sp *rdf2v1.Snippet) []*spdx.Package2_1 {
 	var arrPkg []*spdx.Package2_1
 	for _, a := range spdxdoc.Relationship {
 		if a != nil {
@@ -313,7 +417,23 @@ func transferOtherLicenses(spdxdoc *rdf2v1.Document) []*spdx.OtherLicense2_1 {
 	}
 	return arrOl
 }
-
+func collectOtherLicenses(doc2v1 *spdx.Document2_1) []*rdf2v1.ExtractedLicensingInfo {
+	var arrEl []*rdf2v1.ExtractedLicensingInfo
+	for _, a := range doc2v1.OtherLicenses {
+		if a != nil {
+			stdEl := rdf2v1.ExtractedLicensingInfo{
+				LicenseIdentifier: rdf2v1.Str(a.LicenseIdentifier),
+				ExtractedText:     rdf2v1.Str(a.ExtractedText),
+				// LicenseName:            a.LicenseName,// DISCUSS: more than one fields in RDF but string in standard struct
+				// LicenseCrossReferences: rdf2v1.ValueList(a),//DISCUSS: Not in RDF file
+				LicenseComment: rdf2v1.Str(a.LicenseComment),
+			}
+			pointer := &stdEl
+			arrEl = append(arrEl, pointer)
+		}
+	}
+	return arrEl
+}
 func transferArtifactOfProject(spdxdoc *rdf2v1.Document) []*spdx.ArtifactOfProject2_1 {
 	var arrAop []*spdx.ArtifactOfProject2_1
 	for _, a := range spdxdoc.Relationship {
@@ -341,20 +461,64 @@ func transferArtifactOfProject(spdxdoc *rdf2v1.Document) []*spdx.ArtifactOfProje
 	return arrAop
 }
 
-func transferSnippets(sp []*rdf2v1.Snippet) []*spdx.Snippet2_1 {
+// func collectArtifactOfProject(spdxdoc *spdx.Document2_1) []*rdf2v1.ArtifactOfProject2_1 {
+// 	var arrAop []*spdx.ArtifactOfProject2_1
+// 	for _, a := range spdxdoc.Relationship {
+// 		if a != nil {
+// 			if a.File != nil {
+// 				for _, b := range a.File {
+// 					if b != nil {
+// 						for _, c := range b.Project {
+// 							stdAop := spdx.ArtifactOfProject2_1{
+// 								Name:     c.Name.Val,
+// 								HomePage: c.Homepage.Val,
+// 								URI:      "",
+// 							}
+
+// 							pointer := &stdAop
+// 							arrAop = append(arrAop, pointer)
+// 						}
+// 					}
+
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return arrAop
+// }
+func transferSnippets(sp *rdf2v1.Snippet) []*spdx.Snippet2_1 {
 	var arrSn []*spdx.Snippet2_1
-	for _, a := range sp {
-		if a != nil {
-			stdSn := spdx.Snippet2_1{
-				SnippetLicenseComments:  a.SnippetLicenseComments.Val,
-				SnippetCopyrightText:    a.SnippetCopyrightText.Val,
-				SnippetLicenseConcluded: a.SnippetLicenseConcluded.Val, //DISCUSS: Not in RDF file
-				SnippetComment:          a.SnippetComment.Val,
-				// LicenseInfoInSnippet:    a.LicenseInfoInSnippet, // DISCUSS: more than one fields in RDF but string in standard struct
-			}
-			pointer := &stdSn
-			arrSn = append(arrSn, pointer)
+	// for _, a := range sp {
+	if sp != nil {
+		stdSn := spdx.Snippet2_1{
+			SnippetLicenseComments:  sp.SnippetLicenseComments.Val,
+			SnippetCopyrightText:    sp.SnippetCopyrightText.Val,
+			SnippetLicenseConcluded: sp.SnippetLicenseConcluded.Val, //DISCUSS: Not in RDF file
+			SnippetComment:          sp.SnippetComment.Val,
+			// LicenseInfoInSnippet:    a.LicenseInfoInSnippet, // DISCUSS: more than one fields in RDF but string in standard struct
 		}
+		pointer := &stdSn
+		arrSn = append(arrSn, pointer)
 	}
+	// }
+	return arrSn
+}
+
+func collectSnippets(sp *spdx.Snippet2_1) []*rdf2v1.Snippet {
+	var arrSn []*rdf2v1.Snippet
+	// for _, a := range sp {
+	if sp != nil {
+		stdSn := rdf2v1.Snippet{
+			SnippetLicenseComments:  rdf2v1.Str(sp.SnippetLicenseComments),
+			SnippetCopyrightText:    rdf2v1.Str(sp.SnippetCopyrightText),
+			SnippetLicenseConcluded: rdf2v1.Str(sp.SnippetLicenseConcluded), //DISCUSS: Not in RDF file
+			SnippetComment:          rdf2v1.Str(sp.SnippetComment),
+			// LicenseInfoInSnippet:    a.LicenseInfoInSnippet, // DISCUSS: more than one fields in RDF but string in standard struct
+		}
+		pointer := &stdSn
+		arrSn = append(arrSn, pointer)
+	}
+	// }
 	return arrSn
 }
